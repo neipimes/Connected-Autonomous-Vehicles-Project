00f6ff67d9f53b1bac0a84bcb30ac21cc3759c3a
Chereska <jac07app@gmail.com>
2025-02-18 15:36:49 +0800

    Adjusted buttons and columns to fill out buttons width.

--- code/WebUI/GUI.py
+++ code/WebUI/GUI.py
@@ -1,14 +1,21 @@
 from nicegui import ui
 
-ui.dark_mode().enable()
+#ui.dark_mode().enable()
+
+ui.colors()
+
+ui.add_head_html('<style>body {background-color: #202020; }</style>')
 
 with ui.column().classes("w-full items-center"):
     with ui.row(align_items="center"):
         ui.label("CAV Attack Panel").style('font-size: 48px; font-weight: bold; color: #ffffff;')
-    with ui.row(align_items="center").style('padding-top: 48px;'):
-        ui.button(text="Attack 1")
-        ui.button(text="Attack 2")
-        ui.button(text="Attack 3")
+    with ui.row().style('padding-top: 12px;').classes('w-full justify-center'):
+        with ui.column(align_items="center").classes('col'):
+            ui.button(text="Attack 1").classes("w-full")
+        with ui.column(align_items="center").classes('col'):
+            ui.button(text="Attack 2").classes("w-full")
+        with ui.column(align_items="center").classes('col'):
+            ui.button(text="Attack 3").classes("w-full")
 
 
 
