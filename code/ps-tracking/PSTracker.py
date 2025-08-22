1638963013a1ce5128f699ce56f064ce0a8feb3d
Chereska <jac07app@gmail.com>
2025-08-22 22:23:46 +0800

    Buffer change.

--- code/ps-tracking/PSTracker.py
+++ code/ps-tracking/PSTracker.py
@@ -205,7 +205,7 @@ class PSTracker:
 
             # Continuously read lidar scans and run PSO
             if noLidar == False:
-                for scan in self.lidar.iter_scans(max_buf_meas=3000): # Increased buffer size for initial setup.
+                for scan in self.lidar.iter_scans(max_buf_meas=100000): # Increased buffer size for initial setup.
 
                     # Convert scan to numpy array
                     lidarScan = np.array(scan)
