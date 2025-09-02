8acbea95acef9f367543e73cd585ba03b2457da5
Chereska <jac07app@gmail.com>
2025-09-02 12:12:03 +0800

    Readded parallelisation of the particle updating and cost calculations, as well as particle initialisation.

--- code/ps-tracking/PSO.py
+++ code/ps-tracking/PSO.py
@@ -1,6 +1,7 @@
 from Particle import Particle
 import numpy as np
 import copy, time
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 class PSO:
     def __init__(self, swarmSize: int, w: float, c1: float, c2: float, 
@@ -23,24 +24,19 @@ class PSO:
         self.particles = []
         self.best_particle = None
 
-        # Initialize particles
-        for _ in range(self.swarmSize):
-            # Creates particles with random positions and angles normally distributed around the IMU readings. 
-            # Noise values are representative of the percentage error in the middle 95% (2 standard deviations) of bias values.
-            # Reading (mean) is multiplied by the noise value then divided by two to get the standard deviation.
-            x = np.random.normal(imuXReading, abs(imuXReading) * abs(xNoise) / 2)
-            y = np.random.normal(imuYReading, abs(imuYReading) * abs(yNoise) / 2)
-            angle = np.random.normal(imuAngleReading, abs(imuAngleReading) * abs(angleNoise) / 2)
-            particle = Particle(x, y, angle)
-            self.particles.append(particle)
-
-        # Calculate costs for each particle
-        for particle in self.particles:
-            cost = particle.calcCost(self.oldLidarScan, self.newLidarScan, self.sections)
-            if self.best_particle is None or cost < self.best_particle.cost:
-                # Create a new copy of the best particle and set its cost.
-                self.best_particle = Particle(copy.deepcopy(particle.x), copy.deepcopy(particle.y), copy.deepcopy(particle.angle))
-                self.best_particle.cost = cost
+        # Parallelize particle initialization
+        with ProcessPoolExecutor() as executor:
+            futures = [
+                executor.submit(self._initialize_particle, imuXReading, imuYReading, imuAngleReading, xNoise, yNoise, angleNoise)
+                for _ in range(self.swarmSize)
+            ]
+            for future in futures:
+                particle, cost = future.result()
+                self.particles.append(particle)
+                if self.best_particle is None or cost < self.best_particle.cost:
+                    # Create a new copy of the best particle and set its cost.
+                    self.best_particle = Particle(copy.deepcopy(particle.x), copy.deepcopy(particle.y), copy.deepcopy(particle.angle))
+                    self.best_particle.cost = cost
 
         self.initTime = time.time() - self.trueStartTime
 
@@ -48,29 +44,48 @@ class PSO:
         # How long we have left to run the PSO algorithm due to Lidar constraints.
         # Remaining time should be a conservative estimate to ensure we don't exceed the target time.
 
+    def _initialize_particle(self, imuXReading, imuYReading, imuAngleReading, xNoise, yNoise, angleNoise):
+        """Helper function to initialize a single particle."""
+        x = np.random.normal(imuXReading, abs(imuXReading) * abs(xNoise) / 2)
+        y = np.random.normal(imuYReading, abs(imuYReading) * abs(yNoise) / 2)
+        angle = np.random.normal(imuAngleReading, abs(imuAngleReading) * abs(angleNoise) / 2)
+        particle = Particle(x, y, angle)
+        cost = particle.calcCost(self.oldLidarScan, self.newLidarScan, self.sections)
+        return particle, cost
+
+    def _update_particle(self, particle):
+        """Helper function to update a single particle's velocity, position, and cost."""
+        particle.updateVelocity(self.best_particle, self.w, self.c1, self.c2)
+        particle.updatePosition()
+        cost = particle.calcCost(self.oldLidarScan, self.newLidarScan, self.sections)
+        return particle, cost
+
     def run(self, analytics: bool = False):
         startTime = time.time()
         lastIterTime = 0.0
         iterCount = 0
-        while time.time() - startTime < self.remainingTime - lastIterTime: 
+        while time.time() - startTime < self.remainingTime - lastIterTime:
             # Run until the remaining time is up and a new scan will be available.
             # Don't start a new iteration if the last one took long enough that it would exceed the remaining time.
             iterationRunTime = time.time()
             iterCount += 1
-            for particle in self.particles:
-                # Update particle velocity and position
-                particle.updateVelocity(self.best_particle, self.w, self.c1, self.c2)
-                particle.updatePosition()
-                # Calculate cost for the new position
-                cost = particle.calcCost(self.oldLidarScan, self.newLidarScan, self.sections)
-                if self.best_particle is None or cost < self.best_particle.cost:
-                    self.best_particle = Particle(copy.deepcopy(particle.x), copy.deepcopy(particle.y), copy.deepcopy(particle.angle))
-                    self.best_particle.cost = cost
+
+            # Parallelize particle updates and cost calculations
+            with ProcessPoolExecutor() as executor:
+                futures = {executor.submit(self._update_particle, particle): particle for particle in self.particles}
+                for future in as_completed(futures):
+                    particle, cost = future.result()
+                    if self.best_particle is None or cost < self.best_particle.cost:
+                        # Update the best particle in a thread-safe manner
+                        self.best_particle = Particle(copy.deepcopy(particle.x), copy.deepcopy(particle.y), copy.deepcopy(particle.angle))
+                        self.best_particle.cost = cost
+
             lastIterTime = time.time() - iterationRunTime
+
         totalTime = time.time() - startTime
         trueTotalTime = time.time() - self.trueStartTime
 
-        # Return the best particle's position, angle, cost, total time taken and iteration count.
+        # Return the best particle's position, angle, cost, total time taken, and iteration count.
         return ({'x': self.best_particle.x, 'y': self.best_particle.y, 'angle': self.best_particle.angle,
                 'cost': self.best_particle.cost, 'totalTime': totalTime, 'iterCount': iterCount,
                 'trueTotalTime': trueTotalTime, 'initTime': self.initTime})
